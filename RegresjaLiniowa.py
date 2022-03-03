35baea99bf3d22de82e1cad9e17d4c461f0a4064
poglodjan <100129357+poglodjan@users.noreply.github.com>
2022-03-03 23:35:17 +0100

    Update RegresjaLiniowa.py

--- RegresjaLiniowa.py
+++ RegresjaLiniowa.py
@@ -5,9 +5,9 @@ def oblicz(key,a,b):
     print(f"Przy sile {key} newtonów robot będzie miał uszkodzenia na poziomie około {y}%")
 
 def main():
-    dane_x=[200,160,500,311,863.5]
+    dane_x=[200,160,500,311,863.5] #wpisywanie danych z testów do tablicy
     dane_y=[10,9,28,22,42]
-    a = 0.044
+    a = 0.044 #wyliczone za pomocą wzoru na ten współczynnik
     b = 3.24
     key=int(input())
     oblicz(key,a,b)
@@ -15,7 +15,7 @@ def main():
     # wykres rozproszenia:
     plt.scatter(dane_x, dane_y)
 
-    # rysowanie odcinka [xmin, xmax], [ymin, ymax]:
+    # rysowanie najlepiej dopasowanego odcinka do danych współczynników a,b
     plt.plot([0, 1000], [b+a*(0), b+a*1000], color="red")
     plt.show()
 
