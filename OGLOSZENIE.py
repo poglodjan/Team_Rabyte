e7fcc927b881713b53d1d98018cf51c0993cde51
poglodjan <100129357+poglodjan@users.noreply.github.com>
2022-03-06 14:08:06 +0100

    Create OGLOSZENIE.py

---
+++ OGLOSZENIE.py
@@ -0,0 +1,3 @@
+#Zmieniliśmy dla was githuba!
+#Teraz wszystkie projekty dostępne są pod linkiem:
+#https://github.com/Rabyte-association/Kolo_Zamoyski/blob/main/Badanie%20uszkodze%C5%84%20robota.xlsx
